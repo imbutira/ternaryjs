2609e7bb7992c6617ed6f31971af87902354b53e
epa <epaphrasjohn4@gmail.com>
2026-05-13 02:11:56 +0300

    this is a simple theme selector for a javascript project.

---
+++ theme.js
@@ -0,0 +1,34 @@
+const select = document.querySelector("select");
+const html = document.querySelector("html");
+
+select.addEventListener("change", () => {
+  const choice = select.value;
+
+  switch (choice) {
+  case "white":
+      update("white", "black")
+      break;
+      
+    case "black":
+    update("black", "white")
+    break;
+
+      case "purple":
+    update("purple", "white")
+    break;
+
+      case "yellow":
+    update("yellow", "black")
+    break;
+
+      case "psychedelic":
+    update("lime", "black")
+    break;
+  }
+}
+  );
+
+function update(bgColor, textColor) {
+  html.style.backgroundColor = bgColor;
+  html.style.color = textColor;
+}
